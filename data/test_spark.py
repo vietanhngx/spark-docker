d134541cb36b8af38dc80395d855e9a479cefed1
Nguyen Viet Anh <ngvietanh240605@gmail.com>
2025-11-26 23:24:56 +0700

    update

--- data/test_spark.py
+++
@@ -1,6 +0,0 @@
-from pyspark.sql import SparkSession
-import random
-
-spark = SparkSession.builder.appName("TestApp").getOrCreate()
-print(">>> HELLO SPARK! Phien ban cua ban la: ", spark.version)
-spark.stop()
